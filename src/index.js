c984d1822c48d48f795a4c9b0383e7b567f1d87a
n-kurasawa <kurasawa.nk.naohiro@gmail.com>
2017-12-18 22:18:58 +0900

    react hello world

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React  from 'react'
+import ReactDOM from 'react-dom'
+import App from './app'
+
+const render = Component => {
+  ReactDOM.render(
+    <Component />,
+    document.getElementById('root'),
+  )
+}
+
+render(App)
+
+// Webpack Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    render(App)
+  })
+}
