5a5a0933626a2fe93f36b45bd9eba3194ff32609
n-kurasawa <kurasawa.nk.naohiro@gmail.com>
2018-03-04 10:43:36 +0900

    add clean

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     app: './src/index.js',
   },
   output: {
-    filename: '[name]-[hash].js',
+    filename: '[name]-[hash].bundle.js',
   },
   resolve: {
     modules: ['src', 'node_modules'],
