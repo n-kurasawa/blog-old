b9c1e9c0cc27cfc6173c2211f62a0d23c785486f
n-kurasawa <kurasawa.nk.naohiro@gmail.com>
2018-07-28 12:51:16 +0900

    functions test

--- src/articleApi.js
+++ src/articleApi.js
@@ -1,11 +1,17 @@
 import firebase from '@firebase/app';
 import '@firebase/database';
+import '@firebase/functions';
 import config from './config';
 
 export default class ArticleApi {
   constructor() {
     firebase.initializeApp(config);
     this.db = firebase.database();
+
+    const helloFirebase = firebase.functions().httpsCallable('helloFirebase');
+    helloFirebase({ text: 'test' }).then(result => {
+      console.log(result);
+    });
   }
 
   all() {
